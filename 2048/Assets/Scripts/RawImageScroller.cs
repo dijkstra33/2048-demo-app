be7fc113687afc49a4362af32695e0630b3059c7
dijkstra33 <omgosu@rambler.ru>
2023-10-31 21:42:31 +1300

    Add scrolling background

---
+++ 2048/Assets/Scripts/RawImageScroller.cs
@@ -0,0 +1,23 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+
+namespace Game
+{
+    public class RawImageScroller : MonoBehaviour
+    {
+        [SerializeField]
+        private RawImage _rawImage;
+
+        [SerializeField]
+        private float _xSpeed;
+
+        [SerializeField]
+        private float _ySpeed;
+
+        private void Update()
+        {
+            _rawImage.uvRect = new Rect(_rawImage.uvRect.position + new Vector2(_xSpeed, _ySpeed) * Time.deltaTime,
+                _rawImage.uvRect.size);
+        }
+    }
+}
